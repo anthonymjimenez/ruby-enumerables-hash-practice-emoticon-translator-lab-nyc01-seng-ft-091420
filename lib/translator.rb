8829b016e4ff2e33ef44b54fe86d9f49b7d88ccc
Anthony Jimenez <anthonyjim96@gmail.com>
2020-09-07 20:09:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,14 +1,15 @@
 # require modules here
 require "yaml"
-def load_library
+def load_library(path)
   # code goes here
-  emoticons = YAML.load_file(File.expand_path('../emoticons.yml', __FILE__))
+  emoticons = YAML.load_file(File.expand_path(path, __FILE__))
   emoticons
 end
 
-p load_library
 def get_japanese_emoticon
   # code goes here
+  emoticons = load_library
+
 end
 
 def get_english_meaning
